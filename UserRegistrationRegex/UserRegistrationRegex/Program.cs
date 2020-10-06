ae847ff71f7612d5c60c7aef22b990b4d59a1fe9
vsk4 <saikumarvadla4@gmail.com>
2020-10-06 22:40:14 +0530

    [VSK]:Refractor added UC1-ValidateFirstName

---
+++ UserRegistrationRegex/UserRegistrationRegex/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UserRegistrationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstName;
+            Console.WriteLine("Enter First Name");
+            firstName = Console.ReadLine();
+            UserRegPattern pattern = new UserRegPattern();
+            bool result = pattern.ValidateFirstName(firstName);
+            Console.WriteLine("Validation Check for first name is " + result);
+        }
+    }
+}
---
+++ UserRegistrationRegex/UserRegistrationRegex/UserRegPattern.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace UserRegistrationRegex
+{
+    class UserRegPattern
+    {
+        string firstNameRegex = "[A-Z]{1}[A-Za-z]{2,}";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, firstNameRegex);
+        }
+    }
+}
