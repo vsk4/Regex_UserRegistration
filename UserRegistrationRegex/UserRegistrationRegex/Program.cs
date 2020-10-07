1a94ed85883ca2442ca687d981bbab1af00233cf
vsk4 <saikumarvadla4@gmail.com>
2020-10-07 11:35:18 +0530

    [VSK]:Refractor added UC3-ValidEmail

--- UserRegistrationRegex/UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/UserRegistrationRegex/Program.cs
@@ -5,7 +5,7 @@ namespace UserRegistrationRegex
     {
         static void Main(string[] args)
         {
-            String firstName, lastName;
+            String firstName, lastName, Email;
             Console.WriteLine("Enter First Name");
             firstName = Console.ReadLine();
             UserRegPattern pattern = new UserRegPattern();
@@ -13,8 +13,14 @@ namespace UserRegistrationRegex
             Console.WriteLine("Validation Check for first name is " + result1);
             Console.WriteLine("Enter Last Name");
             lastName = Console.ReadLine();
-            bool result2 = pattern.ValidateFirstName(lastName);
+            bool result2 = pattern.ValidateLastName(lastName);
             Console.WriteLine("Validation Check for last name is " + result2);
+            Console.WriteLine("Enter Email ID :");
+            Email = Console.ReadLine();
+            bool result3 = pattern.ValidateEmail(Email);
+            Console.WriteLine("Validation Check for email :" + result3);
+
+           
         }
     }
 }
--- UserRegistrationRegex/UserRegistrationRegex/UserRegPattern.cs
+++ UserRegistrationRegex/UserRegistrationRegex/UserRegPattern.cs
@@ -8,6 +8,7 @@ namespace UserRegistrationRegex
     {
         string firstNameRegex = "[A-Z]{1}[A-Za-z]{2,}";
         string lastNameRegex = "[A-Z]{1}[A-Za-z]{2,}";
+        string emailRegex = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, firstNameRegex);
@@ -15,6 +16,10 @@ namespace UserRegistrationRegex
         public bool ValidateLastName(string lastName)
         {
             return Regex.IsMatch(lastName, firstNameRegex);
+        }
+        public bool ValidateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, emailRegex);
         }
     }
 }
