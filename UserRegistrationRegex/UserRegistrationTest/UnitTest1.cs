2f5662425c9b2474c37ce0493ff3ff8f12f1406c
vsk4 <saikumarvadla4@gmail.com>
2020-11-05 17:50:48 +0530

    [VSK]: Added UC11_ParameterizedTestEmail

--- UserRegistrationRegex/UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationRegex/UserRegistrationTest/UnitTest1.cs
@@ -21,6 +21,21 @@ namespace UserRegistrationTest
             Assert.AreEqual(expected, pattern.ValidateMobile(mobileNum));
             Assert.AreEqual(expected, pattern.ValidatePassword(password));
         }
-
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateEmailId(string email)
+        {
+            bool expected = true;
+            UserRegPattern pattern = new UserRegPattern();
+            Assert.AreEqual(expected, pattern.ValidateEmail(email));
+        }
     }
 }
